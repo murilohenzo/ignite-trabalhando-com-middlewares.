04e01eb81867eef3eb6a2d9e018faa310dbbcdc8
murilohenzo <murilohezo@gmail.com>
2021-03-27 18:13:33 -0300

    adiciona solucao

--- src/index.js
+++ src/index.js
@@ -10,19 +10,67 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const userExists = users.find((user) => user.username === username);
+
+  if (!userExists) return response.status(404).json({ messages: 'User not found .'});
+
+  request.user = userExists;
+
+  return next();
+  
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } =  request;
+
+  if (user.pro === false && user.todos.length >= 10) return response.status(403).json({ message: 'error' })
+
+  return next();
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id }  = request.params;
+
+  const validation = validate(id);
+
+  if (!validation) {
+    return response.status(400).json({error:'message'});
+  }
+  
+  const user = users.find(user => user.username === username);
+  
+  if (!user) {
+    return response.status(404).json({ error: 'message' });
+  }
+  
+  const todo = user.todos.find(todo=> todo.id === id );
+  
+  if (!todo) {
+    return response.status(404).json({ error: 'message' });
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const userExist = users.find(user => user.id === id);
+  
+  if (!userExist) {
+    return response.status(404).json({ error: 'messages' });
+  }
+
+  request.user = userExist;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
